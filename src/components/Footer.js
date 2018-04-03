c6091bb6646501176b203487e22b594b68f7ed94
Bernardo <bpl@Bernardos-MacBook-Pro.local>
2018-04-03 20:12:57 -0300

    Footer logos with links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,21 @@ import footerDesk from 'assets/images/footer-desk.png';
 
 export class Footer extends React.Component {
   render() {
-    let logos = [logoPatio, logoUnesco, logoSme].map((logo, i) =>
+    let logos = [
+      {img: logoPatio, url: "http://patiodigital.prefeitura.sp.gov.br/"},
+      {img: logoUnesco, url: "http://www.unesco.org/new/pt/brasilia"},
+      {img: logoSme, url: "http://www.capital.sp.gov.br/cidadao/educacao"}
+    ]
+    let logosList = logos.map((logo, i) =>
         <div className="col-xs-4 logo-div">
-          <div className="logo-div" style={{backgroundImage: `url(${logo})`}} ></div>
+          <a href={logo.url} >
+            <div className="logo-div" style={{backgroundImage: `url(${logo.img})`}} ></div>
+          </a>
         </div>
       );
     return (
       <div className="row footer">
-        {logos}
+        {logosList}
       </div>
     );
   }
