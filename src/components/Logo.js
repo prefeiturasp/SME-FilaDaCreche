798a02bb357fa1aa3b3bca17b212569574b3fb30
Bernardo <bpl@Bernardos-MacBook-Pro.local>
2018-04-03 18:28:31 -0300

    Fallback image for SVG logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from 'logo.svg';
+import logo from 'assets/images/logo.svg';
+import logoImg from 'assets/images/logo.png';
 
 export class Logo extends React.Component {
   render() {
     return (
       <div className="logo-banner text-center">
-        <object data={logo} type="image/svg+xml"></object>
+        <object data={logo} type="image/svg+xml">
+          <img src={logoImg} alt="Fila da Creche - O melhor caminho para sua vaga" />
+        </object>
       </div>
     );
   }
