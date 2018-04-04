dcaa0b38320ba0d920a29c70fcc6cfd60cb275ef
Bernardo <bpl@Bernardos-MacBook-Pro.local>
2018-04-04 16:05:21 -0300

    Shorter alert message

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ export class Alert extends React.Component {
   render() {
     return (
       <div className="alert alert-warning" role="alert" style={{"marginBottom": 0}}>
-        <strong>Atenção!</strong> Este site está ainda em fase de testes. Conheça as melhorias previstas e envie sugestões <a href="https://docs.google.com/forms/d/e/1FAIpQLSdt8YXI3i7RdolEMh-TX1oes5zGpSnbE3Sy3-ioVLJTqvgGVQ/viewform" taget="_blank" style={{"textDecoration": "underline"}}>aqui</a>.
+        <strong>Atenção!</strong> Estamos em fase de testes. Conheça as melhorias previstas e envie sugestões <a href="https://docs.google.com/forms/d/e/1FAIpQLSdt8YXI3i7RdolEMh-TX1oes5zGpSnbE3Sy3-ioVLJTqvgGVQ/viewform" taget="_blank" style={{"textDecoration": "underline"}}>aqui</a>.
       </div>
     )
   }
