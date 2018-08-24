b66755599fd76e8a3372a682064427fb8400548c
Victor Sena Molero <victorsenam@gmail.com>
2018-08-24 10:36:29 -0300

    Fixing export style on Alert.js
    I was declaring the constant `Alert` and exporting it later, that's not
    necessary, I could export it right away, which is cleaner. I'm doing
    that now.

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Alert = () => (
+export const Alert = () => (
   <div className="alert alert-warning" role="alert" style={{"marginBottom": 0}}>
   <strong>Atenção!</strong> Estamos em fase de testes. Conheça as melhorias previstas e envie sugestões <a href="https://docs.google.com/forms/d/e/1FAIpQLSdt8YXI3i7RdolEMh-TX1oes5zGpSnbE3Sy3-ioVLJTqvgGVQ/viewform" taget="_blank" style={{"textDecoration": "underline"}}>aqui</a>.
   </div>
 );
-
-export { Alert };
