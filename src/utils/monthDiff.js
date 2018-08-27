f85b670d809573faca380888095522a0c4f0bf48
Bernardo Loureiro <lourb896@newschool.edu>
2018-08-27 09:45:54 -0300

    Remove unused utils

--- src/utils/monthDiff.js
+++
@@ -1,7 +0,0 @@
-export default function monthDiff(d1, d2) {
-  var months;
-  months = (d2.getFullYear() - d1.getFullYear()) * 12;
-  months -= d1.getMonth();
-  months += d2.getMonth();
-  return months <= 0 ? 0 : months;
-}
