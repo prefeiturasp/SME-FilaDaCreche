2107c883f381f00e854ad98bd364479a910a68d8
Victor Sena Molero <victorsenam@gmail.com>
2018-09-03 15:07:04 -0300

    HomeBanner as stateless component

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Logo } from "./Logo";
+import PropTypes from 'prop-types';
 
-export class HomeBanner extends React.Component {
-  render() {
-    let paragraphsList;
-    if (this.props.paragraphs) {
-      paragraphsList = this.props.paragraphs.map((paragraph, i) =>
+export const HomeBanner = (props)  => (
+  <div className="banner">
+    <Logo />
+    <hr />
+    <ol className="list-ordered">
+      {props.paragraphs ? props.paragraphs.map((paragraph, i) =>
         <li key={'p_' + i}>{paragraph}</li>
-      );
-    }
-    return (
-      <div className="banner">
-        <Logo />
-        <hr />
-        <ol className="list-ordered">{paragraphsList}</ol>
-      </div>
-    );
-  }
-}
+      ) : null}
+    </ol>
+  </div>
+);
+
+HomeBanner.propTypes = {
+  paragraphs : PropTypes.arrayOf(PropTypes.string)
+};
