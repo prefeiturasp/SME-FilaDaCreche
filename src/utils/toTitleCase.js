27d504e2d8ae17fd91ea037c676dcaf7c952b893
Victor Sena Molero <victorsenam@gmail.com>
2018-09-08 13:39:47 -0300

    Altera toTitleCase para tratar acentos
    No formato atual, a Regex trata acentos a partir do locale. O que não tem problema pra produção, mas é um problema pros nossos testes. Eu sugiro mudar a regex para esta nova. Ela trata qualquer sequência de não-espaços contígua como uma palavra e aumenta a caixa da primeira letra. Acredito que isso não cause nenhum problema, um exemplo de diferença é que, da forma antiga, a string ".teste" viraria ".Teste" e agora ela continua ".teste". Mas realmente acho que isso não é um problema.

--- src/utils/toTitleCase.js
+++ src/utils/toTitleCase.js
@@ -1,3 +1,3 @@
 export default function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  return str.replace(/\S+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
