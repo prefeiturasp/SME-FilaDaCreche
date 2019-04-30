13f3799cbe4a2521f906687e3f92348f7cbed6eb
Marcelo Maia <mmaia.cc@gmail.com>
2019-04-30 11:27:47 -0300

    pequeno aprimoramento.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,10 @@
 # docker run  -p 8000:80 -e API_URL="http:\/\/mysite.com\/api\/v1" -e MAP_API_URL="ANOTHER URL" marcelomaia/fila_da_crech:latest
 # try on local:
 # sed -i "s/SCHOOLS_API_ENDPOINT_REPLACE_ME/http:\/\/localhost:8080\/v1/g" build/static/js/main*.js
-# sed -i "s/MAPS_API_ENDPOINT_REPLACE_ME/http:\/\/localhost:777\//g" build/static/js/main*.js
+# sed -i "s/MAPS_API_ENDPOINT_REPLACE_ME/http:\/\/localhost:777/g" build/static/js/main*.js
 
 set -xe
   : "${API_URL?Need an api url}"
-
 sed -i "s/SCHOOLS_API_ENDPOINT_REPLACE_ME/$API_URL/g" /usr/share/nginx/html/static/js/main*.js
 sed -i "s/MAPS_API_ENDPOINT_REPLACE_ME/$MAP_API_URL/g" /usr/share/nginx/html/static/js/main*.js
 
